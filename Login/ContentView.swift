04d2a8514e13253327d646c8b710fb0ec68cc16c
Jan Hovland <jan.hovland@lyse-net>
2019-11-14 23:18:42 +0100

    Update

--- Login/ContentView.swift
+++ Login/ContentView.swift
@@ -9,8 +9,6 @@
 //  Block comment : Ctrl + Cmd + / (on number pad)
 //  Indent        : Ctrl + Cmd + * (on number pad)
 
-
-
 import SwiftUI
 
 struct ContentView: View {
@@ -21,21 +19,12 @@ struct ContentView: View {
     @State var signup = false
     
     var body: some View {
-        
-        ZStack{
-            
-//            LinearGradient(gradient: .init(colors: [Color("1"),Color("2")]), startPoint: .leading, endPoint: .trailing).edgesIgnoringSafeArea(.all)
-            
-            
+        VStack{
             Login(login: $login, signup: $signup, user: $user, pass: $pass)
-
-            
         }.alert(isPresented: $login) {
-            
             Alert(title: Text(self.user), message: Text(self.pass), dismissButton: .none)
         }
         .sheet(isPresented: $signup) {
-            
             signUp(signup: self.$signup)
         }
     }
@@ -55,81 +44,41 @@ struct Login : View {
     @Binding var pass : String
     
     var body : some View{
-        NavigationView {
-            VStack(alignment: .center, spacing: 12, content: {
-                
-                Image("CloudKit").resizable().frame(width: 80, height: 80).padding(.bottom, 15)
-                
-                VStack {
-                
-                    HStack{
-                        
-                        Image(systemName: "person.fill").resizable().frame(width: 20, height: 20)
-                        
-                        TextField("Username", text: $user).padding(.leading, 12).font(.system(size: 20))
-                        
-                    }.padding(12)
-                    // .background(Color("Color"))
-                    .cornerRadius(20)
-                    
-                    HStack{
-                        
-                        Image(systemName: "lock.fill").resizable().frame(width: 15, height: 20).padding(.leading, 3)
-                        
-                        SecureField("Password", text: $pass).padding(.leading, 12).font(.system(size: 20))
-                        
-                    }.padding(12)
-                    //.background(Color("Color"))
-                    .cornerRadius(20)
-                    
-                }
-                
-                Button(action: {
-                    
-                    self.login.toggle()
-                    
-                }) {
-                    
-                    Text("Login") // .foregroundColor(.white).padding().frame(width: 150)
-                    
-                }
-                //.background(LinearGradient(gradient: .init(colors: [Color("1"),Color("2")]), startPoint: .leading, endPoint: .trailing))
+        VStack(alignment: .center, spacing: 12, content: {
+            Image("CloudKit").resizable().frame(width: 80, height: 80).padding(.bottom, 15)
+            HStack{
+                Image(systemName: "person.fill").resizable().frame(width: 20, height: 20)
+                TextField("Username", text: $user).padding(.leading, 12).font(.system(size: 20))
+            }.padding(12)
                 .cornerRadius(20)
-                .shadow(radius: 25)
-                
-    //            Button(action: {
-    //
-    //            }) {
-    //
-    //                Text("Forget password?") // .foregroundColor(.white)
-    //            }
-                
-                VStack{
-                    
-                    Text("Dont have account yet") // .foregroundColor(.white)
-                    
-                    Button(action: {
-                        
-                        
-                        self.signup.toggle()
-                        
-                    }) {
-                        
-                        Text("SignUp")// foregroundColor(.white).padding().frame(width: 150)
-                        
-                    }
-                        // .background(LinearGradient(gradient: .init(colors: [Color("1"),Color("2")]), startPoint: .leading, endPoint: .trailing))
-                        .cornerRadius(20)
-                        .shadow(radius: 25)
-                    
-                }.padding(.top, 35)
-                
-            })
-            .padding(.horizontal, 18)
-      
-        }
+            
+            HStack{
+                Image(systemName: "lock.fill").resizable().frame(width: 15, height: 20).padding(.leading, 3)
+                SecureField("Password", text: $pass).padding(.leading, 12).font(.system(size: 20))
+            }.padding(12)
+                .cornerRadius(20)
+            
+            Button(action: {
+                self.login.toggle()
+            }) {
+                Text("Login")
+            }
+            .cornerRadius(20)
+            .shadow(radius: 25)
+            
+            Text("Dont have account yet")
+            Button(action: {
+                self.signup.toggle()
+            }) {
+                Text("SignUp")
+            }
+            .cornerRadius(20)
+            .shadow(radius: 25)
+            
+            Spacer()
+        })
+        .padding(.horizontal, 18)
     }
-    
 }
 
 struct signUp : View  {
@@ -140,66 +89,36 @@ struct signUp : View  {
     @State var repass : String = ""
     
     var body : some View{
-        
-        
-        ZStack{
+        VStack(alignment: .center, spacing: 22, content: {
+            Image("CloudKit").resizable().frame(width: 80, height: 80).padding(.bottom, 15)
             
-            LinearGradient(gradient: .init(colors: [Color("1"),Color("2")]), startPoint: .leading, endPoint: .trailing).edgesIgnoringSafeArea(.all)
+            HStack{
+                Image(systemName: "person.fill").resizable().frame(width: 20, height: 20)
+                TextField("Username", text: $user).padding(.leading, 12).font(.system(size: 20))
+            }.padding(12)
+                .cornerRadius(20)
             
+            HStack{
+                Image(systemName: "lock.fill").resizable().frame(width: 15, height: 20).padding(.leading, 3)
+                SecureField("Password", text: $repass).padding(.leading, 12).font(.system(size: 20))
+            }.padding(12)
+                .cornerRadius(20)
             
-            VStack(alignment: .center, spacing: 22, content: {
-                     
-                     Image("CloudKit").resizable().frame(width: 80, height: 80).padding(.bottom, 15)
-
-                     HStack{
-                         
-                         Image(systemName: "person.fill").resizable().frame(width: 20, height: 20)
-                         
-                         TextField("Username", text: $user).padding(.leading, 12).font(.system(size: 20))
-                         
-                     }.padding(12)
-                     // .background(Color("Color"))
-                     .cornerRadius(20)
-                     
-                     HStack{
-                         
-                         Image(systemName: "lock.fill").resizable().frame(width: 15, height: 20).padding(.leading, 3)
-                         
-                         SecureField("Password", text: $repass).padding(.leading, 12).font(.system(size: 20))
-                         
-                     }.padding(12)
-                     // .background(Color("Color"))
-                     .cornerRadius(20)
-                     
-                     HStack{
-                         
-                         Image(systemName: "lock.fill").resizable().frame(width: 15, height: 20).padding(.leading, 3)
-                         
-                         SecureField("Re-Password", text: $pass).padding(.leading, 12).font(.system(size: 20))
-                         
-                     }.padding(12)
-                     // .background(Color("Color"))
-                     .cornerRadius(20)
-                     
-                     Button(action: {
-                         
-                         self.signup.toggle()
-                         
-                     }) {
-                         
-                         Text("SignUp") // .foregroundColor(.white).padding().frame(width: 150)
-                         
-                     }
-                     // .background(LinearGradient(gradient: .init(colors: [Color("1"),Color("2")]), startPoint: .leading, endPoint: .trailing))
-                     .cornerRadius(20)
-                     .shadow(radius: 25)
-                     
-                     
-                 })
-                 .padding(.horizontal, 18)
+            HStack{
+                Image(systemName: "lock.fill").resizable().frame(width: 15, height: 20).padding(.leading, 3)
+                SecureField("Re-Password", text: $pass).padding(.leading, 12).font(.system(size: 20))
+            }.padding(12)
+                .cornerRadius(20)
             
-        }
-        
+            Button(action: {
+                self.signup.toggle()
+            }) {
+                Text("SignUp")
+            }
+            .cornerRadius(20)
+            .shadow(radius: 25)
+        })
+        .padding(.horizontal, 18)
     }
 }
 
